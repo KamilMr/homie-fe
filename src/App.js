5056d38a09d956fbbdf5e428e0aa83f970d5628a
kamil <kamil@chicos.life>
2022-04-08 18:00:25 +0200

    Add simple fetch client init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import {useState, useEffect} from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [client, setClients] = useState();
+
+  const fetchData = () => {
+    fetch('http://localhost:1234/ini')
+      .then(res => res.json())
+      .then(res => setClients(res))
+  };
+
+  useEffect(() => {
+    fetchData();
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Homie">
+      {client?.map(c => (
+        <div>{c.name} {c.surname} {c.price + 'zł'} {c.time + ':00'}</div>
+      ))}
     </div>
   );
 }
